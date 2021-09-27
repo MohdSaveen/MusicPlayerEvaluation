90391e03933e1610170400e27067950c114b006e
Saveen <saveen047@gmail.com>
2021-09-27 16:07:58 +0530

    commit4

---
+++ app/src/main/java/com/example/evaluation2/ViewHolderClass.kt
@@ -0,0 +1,19 @@
+package com.example.evaluation2
+
+import android.view.View
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import android.widget.TextView
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.item_layout.view.*
+
+class ViewHolderClass(private val itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+
+    fun setData(resultsDTO: ResultsDTO) {
+       itemView.tvArtistName.text=resultsDTO.artistName
+        itemView.tvSongName.text=resultsDTO.trackName
+
+    }
+
+}
