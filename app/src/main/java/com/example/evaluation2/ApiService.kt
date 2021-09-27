34716b33502de30d64836cbef3b2a33a6efb4f54
Saveen <saveen047@gmail.com>
2021-09-27 15:13:21 +0530

    commit2

---
+++ app/src/main/java/com/example/evaluation2/ApiService.kt
@@ -0,0 +1,11 @@
+package com.example.evaluation2
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ApiService {
+
+    @GET("search")
+    fun getData(@Query("term") string:String):Call<List<ResponseDTO>>
+}
