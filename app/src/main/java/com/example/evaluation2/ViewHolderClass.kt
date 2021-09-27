55e5336d28a988790f5356f98f884590d19668b9
Saveen <saveen047@gmail.com>
2021-09-27 17:33:28 +0530

    commiting last

--- app/src/main/java/com/example/evaluation2/ViewHolderClass.kt
+++ app/src/main/java/com/example/evaluation2/ViewHolderClass.kt
@@ -1,6 +1,7 @@
 package com.example.evaluation2
 
 import android.view.View
+import android.widget.Button
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import android.widget.TextView
@@ -10,10 +11,14 @@ import kotlinx.android.synthetic.main.item_layout.view.*
 class ViewHolderClass(private val itemView: View) : RecyclerView.ViewHolder(itemView) {
 
 
+
     fun setData(resultsDTO: ResultsDTO) {
        itemView.tvArtistName.text=resultsDTO.artistName
         itemView.tvSongName.text=resultsDTO.trackName
         Glide.with(itemView.context).load(resultsDTO.previewUrl).into(itemView.ivSongPic)
+
     }
 
 }
+
+
